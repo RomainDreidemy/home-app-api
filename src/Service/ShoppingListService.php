f10056392f1a08eef964b0ee5d56c4e9989f312e
RomainDreidemy <dreidemyromain@gmail.com>
2021-03-27 18:47:13 +0100

    php 7.4 => 8.0

--- src/Service/ShoppingListService.php
+++ src/Service/ShoppingListService.php
@@ -12,30 +12,7 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ShoppingListService
 {
-    private $manager;
-
-    public function __construct(EntityManagerInterface $manager, private ValidatorInterface $validator)
-    {
-        $this->manager = $manager;
-    }
-
-    public function getAllByUserId(Home $home): array
-    {
-        $shoppingLists = $this->manager->getRepository(ShoppingList::class)->findByHome($home);
-        $shoppingListsReturn = [];
-
-        /** @var ShoppingList $shoppingList */
-        foreach ($shoppingLists as $shoppingList) {
-            $shoppingListsReturn[] = [
-                'id' => $shoppingList->getId(),
-                'name' => $shoppingList->getName(),
-                'created_at' => $shoppingList->getCreatedAt()->format('d/m/Y'),
-                'modified_at' => $shoppingList->getModifiedAt() !== null ? $shoppingList->getModifiedAt()->format('d/m/Y') : null
-            ];
-        }
-
-        return $shoppingListsReturn;
-    }
+    public function __construct(private EntityManagerInterface $manager, private ValidatorInterface $validator){}
 
     public function create(string $name, int $home_id): ErrorHelper
     {
@@ -61,7 +38,7 @@ class ShoppingListService
         return new ErrorHelper(status: true, message: 'La liste de course à été créée.');
     }
 
-    public function update(string $name)
+    public function update(string $name): ErrorHelper
     {
         $shoppingList = (new ShoppingList())
             ->setName($name)
@@ -79,13 +56,18 @@ class ShoppingListService
         return new ErrorHelper(status: true, message: 'La liste de course a été modifié.');
     }
 
-    public function remove(ShoppingList $shoppingList)
+    public function remove(ShoppingList $shoppingList): ErrorHelper
     {
-        $this->manager->remove($shoppingList);
-        $this->manager->flush();
+        try {
+            $this->manager->remove($shoppingList);
+            $this->manager->flush();
+            return new ErrorHelper(true, 'La liste de course est supprimé.');
+        }catch (\Exception $exception){
+            return new ErrorHelper(true, $exception->getMessage());
+        }
     }
 
-    public function clone(int $shopping_list_id)
+    public function clone(int $shopping_list_id): ErrorHelper
     {
         /** @var ShoppingList $old */
         $old = $this->manager->getRepository(ShoppingList::class)->find($shopping_list_id);
@@ -94,5 +76,7 @@ class ShoppingListService
 
         $this->manager->persist($new);
         $this->manager->flush();
+
+        return new ErrorHelper(true, 'La liste de course est clonée.', $new);
     }
 }
